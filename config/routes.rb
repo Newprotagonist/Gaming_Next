76f6f54207bd8af20fa604cc90e9b4f2b6717c48
Vincent Ramos <vince9322@gmail.com>
2022-05-30 22:58:23 +0200

    routes, favorites controller & started index for favorites

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :favorites, only: %i[index create update]
+  resources :wishlists, only: %i[index create update] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: %i[destroy]
+  resources :games, only: :show
 end
