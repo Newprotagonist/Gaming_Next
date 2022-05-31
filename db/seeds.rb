a29f8b119e82f732dc5f899feac22ffee952e421
Timothee Peroulakis <timperoulakis@gmail.com>
2022-05-31 11:25:07 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,20 +3,25 @@ require 'json'
 puts "Destroying all games..."
 Game.destroy_all
 puts "Games destroyed"
-
-puts "Parsing games seed..."
-games = JSON.parse(File.read(File.join(__dir__, "seeds/some_games.json")))
-puts "Creating games..."
-games.each do |game|
-  Game.create(
-    name: game["name"],
-    summary: game["summary"],
-    screenshots: game["screenshots"]&.map { |s| s["url"]&.prepend("https:")&.sub("t_thumb", "t_1080p") },
-    videos: game["videos"]&.map { |v| "https://youtu.be/#{v['video_id']}" },
-    cover: game["cover"] ? game["cover"]["url"]&.prepend("https:")&.sub("t_thumb", "t_1080p") : nil,
-    platforms: game["platforms"]&.map { |p| p["name"] },
-    tags: game["tags"],
-    rating: game["aggregated_rating"]
-  )
+files = Dir.children(File.join(__dir__, "seeds/data")).sort_by { |f| f[/\d+/].to_i }
+n = 0
+start = Time.now
+files.each do |file|
+  # print "\rParsing file: #{file}"
+  games = JSON.parse(File.read(File.join(__dir__, "seeds/data/#{file}")))
+  print "\rCreating games: #{n} - #{file[/177/] ? n + 357 : n + 500}"
+  n += 500
+  games.each do |game|
+    Game.create(
+      name: game["name"],
+      summary: game["summary"],
+      screenshots: game["screenshots"]&.map { |s| s["url"]&.prepend("https:")&.sub("t_thumb", "t_1080p") },
+      videos: game["videos"]&.map { |v| "https://youtu.be/#{v['video_id']}" },
+      cover: game["cover"] ? game["cover"]["url"]&.prepend("https:")&.sub("t_thumb", "t_1080p") : nil,
+      platforms: game["platforms"]&.map { |p| p["name"] },
+      tags: game["tags"],
+      rating: game["aggregated_rating"]
+    )
+  end
 end
-puts "Done"
+puts "\nDone in #{Time.now - start}s"
