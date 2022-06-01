6026e84bb54aebe56e571e06fe8654c99561f562
Timothee Peroulakis <timperoulakis@gmail.com>
2022-06-01 15:20:56 +0200

    final

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_30_154526) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_31_144209) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_30_154526) do
     t.float "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "release_date"
+    t.string "developer"
+    t.string "franchise"
+    t.string "game_modes", array: true
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,10 +56,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_30_154526) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "game_id", null: false
+    t.index ["game_id"], name: "index_wishlists_on_game_id"
     t.index ["user_id"], name: "index_wishlists_on_user_id"
   end
 
   add_foreign_key "favorites", "games"
   add_foreign_key "favorites", "users"
+  add_foreign_key "wishlists", "games"
   add_foreign_key "wishlists", "users"
 end
