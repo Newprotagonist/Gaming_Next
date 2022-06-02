2eaa6272b5bc6fcf6f866257f862e42d9a2904e9
Bastien Grandjean <bastien.grandjean.75@gmail.com>
2022-06-02 16:55:23 +0200

    push to heroku

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,13 +1,13 @@
 class SuggestionsController < ApplicationController
   def index
     skip_policy_scope
-    tags = current_user.favorites.map(&:game).pluck(:tags).flatten
+    @tags = current_user.favorites.map(&:game).pluck(:tags).flatten
     @suggestions = []
-    @suggestions = Game.with_any_tags(tags).to_a
+    @suggestions = Game.with_any_tags(@tags).to_a
     current_user.favorites.map(&:game).each { |game| @suggestions.delete(game) }
     # @suggestions = @suggestions.flatten.uniq.select do |suggestion|
     #   (suggestion.tags & tags).size > suggestion.tags.size / 1.5
     # end
-    @suggestions = @suggestions.sort_by { |game| -(game.tags & tags).size.fdiv((game.tags + tags).uniq.size) }.first(20)
+    @suggestions = @suggestions.sort_by { |game| -(game.tags & @tags).size.fdiv((game.tags + @tags).uniq.size) }.first(20)
   end
 end
