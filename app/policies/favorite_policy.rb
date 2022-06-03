0241f0bb55f6b8bb076662cb799fa1b8613ce9f5
Timothee Peroulakis <timperoulakis@gmail.com>
2022-06-03 14:59:22 +0200

    hello 2

--- app/policies/favorite_policy.rb
+++ app/policies/favorite_policy.rb
@@ -2,7 +2,7 @@ class FavoritePolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-      scope.where(user: user)
+      scope.where(user: user).order(created_at: :desc)
     end
   end
 
