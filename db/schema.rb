0e97270d83449b67d050c513fba8d4067847985c
Dagmara Korta <dagmara.korta@gmail.com>
2022-06-03 14:40:37 +0200

    minor fix - link on the favorite card didnt work before

--- db/schema.rb
+++ db/schema.rb
@@ -92,6 +92,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_03_053653) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "game_id", null: false
+    t.integer "sort"
     t.index ["game_id"], name: "index_wishlists_on_game_id"
     t.index ["user_id", "game_id"], name: "index_wishlists_on_user_id_and_game_id", unique: true
     t.index ["user_id"], name: "index_wishlists_on_user_id"
